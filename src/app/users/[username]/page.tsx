7c71c82c64f63ebb34036b9514fc04e14d0143ff
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-07-07 23:57:38 +0600

    added EditUser

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -1,4 +1,5 @@
 import CardList from "@/components/CardList";
+import EditUser from "@/components/EditUser";
 import { Badge } from "@/components/ui/badge";
 import {
   Breadcrumb,
@@ -8,12 +9,14 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
 import { Progress } from "@/components/ui/progress";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
 import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
 
 const SingleUserPage = () => {
@@ -104,7 +107,15 @@ const SingleUserPage = () => {
           </div>
           {/* INFORMATION CONTAINER */}
           <div className="bg-primary-foreground p-4 rounded-lg">
-            <h1 className="text-xl font-semibold mb-4">User Information</h1>
+            <div className="flex items-center justify-between">
+              <h1 className="text-xl font-semibold mb-4">User Information</h1>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button>Edit User</Button>
+                </SheetTrigger>
+                <EditUser />
+              </Sheet>
+            </div>
             <div className="space-y-4">
               <div className="flex flex-col gap-2 mb-6">
                 <p className="text-sm text-muted-foreground">
